13039ccf4ed9fe2d0303ea2bdab256545b36c98e
Vinicius Sakai <vinicius.sakai@tuta.io>
2024-01-03 18:47:49 -0300

    fix: next image without width and height

--- components/ui/techCard.tsx
+++ components/ui/techCard.tsx
@@ -12,32 +12,32 @@ interface TechCardProps {
 export function TechCard({ tech }: TechCardProps) {
   const { image, link, title } = tech
   return (
-    <motion.div
-      variants={{
-        hidden: {
-          opacity: 0,
-          x: 75,
-        },
+    <a href={link} className="w-full md:w-fit">
+      <motion.div
+        variants={{
+          hidden: {
+            opacity: 0,
+            x: 75,
+          },
 
-        visible: {
-          opacity: 1,
-          x: 0,
-        },
-      }}
-      initial="hidden"
-      animate="visible"
-      transition={{ duration: 0.5 }}
-    >
-      <a href={link} className="w-full md:w-fit">
+          visible: {
+            opacity: 1,
+            x: 0,
+          },
+        }}
+        initial="hidden"
+        animate="visible"
+        transition={{ duration: 0.5 }}
+      >
         <div className="flex w-full items-center justify-between gap-4 rounded-md bg-slate-50  p-3 md:w-52 dark:bg-slate-900">
           <div className="grid h-16 w-16 place-content-center rounded-md bg-muted p-2 dark:bg-slate-600">
-            <Image src={image} alt={title} className="" />
+            <Image src={image} alt={title} height={48} width={48} />
           </div>
           <h3 className=" text-md flex-1 text-center font-semibold sm:w-full">
             {title}
           </h3>
         </div>
-      </a>
-    </motion.div>
+      </motion.div>
+    </a>
   )
 }
