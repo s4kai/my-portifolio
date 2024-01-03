41760127df8e11befdb60ace54a5a51655f7ae8b
Vinicius Sakai <vinicius.sakai@tuta.io>
2024-01-03 16:47:39 -0300

    fix: tech card on dark mode

--- components/ui/techCard.tsx
+++ components/ui/techCard.tsx
@@ -28,8 +28,8 @@ export function TechCard({ tech }: TechCardProps) {
       transition={{ duration: 0.5 }}
     >
       <a href={link} className="md:w-fit w-full">
-        <div className="flex bg-slate-50 rounded-md justify-between items-center p-3 gap-4  w-full md:w-52">
-          <div className="bg-muted h-16 w-16 rounded-md p-2 grid place-content-center">
+        <div className="flex bg-slate-50 rounded-md justify-between items-center p-3 gap-4  w-full md:w-52 dark:bg-slate-900">
+          <div className="bg-muted h-16 w-16 rounded-md p-2 grid place-content-center dark:bg-slate-600">
             <img src={image} alt={title} className="" />
           </div>
           <h3 className=" flex-1 text-md font-semibold text-center sm:w-full">
